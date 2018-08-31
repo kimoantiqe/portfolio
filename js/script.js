d51bb1ee1ccdce3b81e88325ebf5f89bb6a518cc
Karim Hesham <kimoantiqe@hotmail.com>
2018-09-01 03:24:25 +0400

    Bouncy scroll , footer edit , mobile enhancments

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
 		//Wait for intro animation to complete
 		$("#bigImg").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
-			$(this).removeClass("animated tada");
+		  $(this).removeClass("animated tada");
+		});
+
+		$('#head').on('click',function () {
+			 $('#bigImg').addClass("animated tada");
 		});
 
 		//On mouse entering image vacinity do the wobble animation
 		$("#bigImg").mouseenter(function(event) {
-			$(this).addClass("animated wobble");
+		  $(this).addClass("animated wobble");
 		});
 
 		//Wait for the wobble to finish and rest
 		$("#bigImg").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
-			$(this).removeClass("animated wobble");
+		  $(this).removeClass("animated wobble");
 		});
 
-		$(function() {
-			$(window).scroll(function() {
-				var scroll = $(window).scrollTop();
-				if (scroll >= 97) {
-					$("#nav-menue").addClass('bg-light shadow-sm');
-				} else {
-					$("#nav-menue").removeClass("bg-light shadow-sm");
-				}
-			});
+		//Menue  change color mechanics
+		$(function() { // change on scroll
+		  $(window).scroll(function() {
+		    var scroll = $(window).scrollTop();
+		    if (scroll >= 97) {
+		      $("#nav-menue").addClass('nav-light shadow-sm');
+		    } else {
+		      $("#nav-menue").removeClass("nav-light shadow-sm");
+		    }
+		  });
 		});
 
+		//refresh position check
+		if ($(window).scrollTop() >= 97) {
+		  $("#nav-menue").addClass('nav-light shadow-sm');
+		} else {
+		  $("#nav-menue").removeClass("nav-light shadow-sm");
+		}
+
+		//Detect a menue click
+		$('a.bouncyScroll').on('click', function(event) {
+		  $('html, body').stop().animate({
+		    //Move to href location
+		    scrollTop: $($(this).attr('href')).offset().top - 50
+		  }, 1100, 'easeInOutExpo'); //Using easing plugin
+		  event.preventDefault();
+		});
